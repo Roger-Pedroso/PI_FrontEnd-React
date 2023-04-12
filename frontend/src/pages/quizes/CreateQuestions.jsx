029d69474b48fe63698fdf2c2779e2e7f36d81e3
yNanneRy <renangealves@gmail.com>
2023-04-11 22:18:45 -0300

    Comecei a fazer a adição de alternativas

--- frontend/src/pages/quizes/CreateQuestions.jsx
+++ frontend/src/pages/quizes/CreateQuestions.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-return-assign */
 /* eslint-disable linebreak-style */
 import React, { useState } from 'react';
 import { InputText } from 'primereact/inputtext';
@@ -8,6 +9,15 @@ import Spans from '../../components/Spans';
 export default function CreateQuestions() {
   const [alt, setAlt] = useState(false);
   const [mult, setMult] = useState(false);
+  const [question, setQuestion] = useState({
+    nome: '',
+    descricao: '',
+  });
+  // eslint-disable-next-line no-unused-vars
+  const [type, setType] = useState(null);
+  const onChange = (e) => {
+    setQuestion({ ...question, [e.target.name]: e.target.value });
+  };
 
   return (
     <div>
@@ -25,28 +35,29 @@ export default function CreateQuestions() {
         >
           <div className="p-inputgroup">
             <Spans icon="pi pi-question" />
-            <InputText name="Nome da Questão" placeholder="Nome da Questão" />
+            <InputText name="nome" placeholder="Nome da Questão" onChange={(e) => onChange(e)} />
           </div>
           <div className="p-inputgroup" style={{ height: '100px' }}>
             <Spans icon="pi pi-align-justify" />
-            <InputText name="Descrição (OPCIONAL)" placeholder="Descrição (OPCIONAL)" />
+            <InputText name="descricao" placeholder="Descrição (OPCIONAL)" onChange={(e) => onChange(e)} />
           </div>
 
           <div className=" flex justify-content-center">
-            <Button id="botoes" label="0 a 10" />
-            <Button id="botoes" label="Alternativa" onClick={() => setAlt(true)} />
+            <Button id="botoes" label="0 a 10" onClick={() => setType('0_a_10')} />
+            <Button id="botoes" label="Alternativa" onClick={() => { setType('alternativa'); setAlt(true); }} />
             <Dialog id="alt" header="Alternativa" headerStyle={{ position: 'relative', textAlign: 'center' }} justifyContent="center" visible={alt} maximizable style={{ width: '50vw' }} onHide={() => setAlt(false)}>
-              <div>
-                <InputText placeholder="teste" />
+              <div className=" flex justify-content-center gap-3">
+                <InputText placeholder="Opcão" />
+                <Button id="adicao" icon="pi pi-plus" rounded outlined aria-label="Filter" />
               </div>
             </Dialog>
-            <Button id="botoes" label="Múltipla Escolha" onClick={() => setMult(true)} />
+            <Button id="botoes" label="Múltipla Escolha" onClick={() => { setType('multipla_escolha'); setMult(true); }} />
             <Dialog id="mult" header="Múltipla Escolha" headerStyle={{ position: 'relative', textAlign: 'center' }} justifyContent="center" visible={mult} maximizable style={{ width: '50vw' }} onHide={() => setMult(false)}>
               <div>
                 <InputText placeholder="teste2" />
               </div>
             </Dialog>
-            <Button id="botoes" label="Aberta" />
+            <Button id="botoes" label="Aberta" onClick={() => setType('aberta')} />
           </div>
         </div>
       </div>
@@ -58,7 +69,8 @@ export default function CreateQuestions() {
           justifyContent: 'end',
         }}
       >
-        <Button label="Limpar" icon="pi pi-check" />
+
+        <Button id="botao_limpar" label="Limpar" icon="pi pi-check" />
         <Button label="Listar" icon="pi pi-check" />
         <Button label="Salvar" icon="pi pi-check" />
       </div>
