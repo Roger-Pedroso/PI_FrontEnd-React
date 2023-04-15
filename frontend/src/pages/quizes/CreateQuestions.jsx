738eafa9b6d7843b0fa55cfb31c82cc6c38c61a9
yNanneRy <renangealves@gmail.com>
2023-04-14 22:12:56 -0300

    Avançei pra krai

--- frontend/src/pages/quizes/CreateQuestions.jsx
+++ frontend/src/pages/quizes/CreateQuestions.jsx
@@ -18,6 +18,31 @@ export default function CreateQuestions() {
   const onChange = (e) => {
     setQuestion({ ...question, [e.target.name]: e.target.value });
   };
+  const [save, setSave] = useState(true);
+  const [alternativas, setAlternativas] = useState([{ id: 1, value: '' }]);
+  const addAlternativa = () => {
+    const novaAlternativa = { id: Date.now(), value: '' };
+    setAlternativas([...alternativas, novaAlternativa]);
+  };
+  const handleAlternativaChange = (event, id) => {
+    const novasAlternativas = [...alternativas];
+    const index = alternativas.findIndex((alter) => alter.id === id);
+    novasAlternativas[index].value = event.target.value;
+    setAlternativas(novasAlternativas);
+  };
+  const handleDeleteAlternativa = (index) => {
+    const novasAlternativas = [...alternativas];
+    novasAlternativas.splice(index, 1);
+    setAlternativas(novasAlternativas);
+  };
+
+  // eslint-disable-next-line no-unused-vars
+  const [isTitleEmpty, setIsTitleEmpty] = useState(false);
+  function handleTitleChange(e) {
+    setIsTitleEmpty(e.target.value === '');
+  }
+
+  const vazio = alternativas.length === 0;
 
   return (
     <div>
@@ -35,7 +60,7 @@ export default function CreateQuestions() {
         >
           <div className="p-inputgroup">
             <Spans icon="pi pi-question" />
-            <InputText name="nome" placeholder="Nome da Questão" onChange={(e) => onChange(e)} />
+            <InputText name="nome" placeholder="Nome da Questão (OBRIGATÓRIO)" onChange={(e) => { onChange(e); handleTitleChange(e); }} required />
           </div>
           <div className="p-inputgroup" style={{ height: '100px' }}>
             <Spans icon="pi pi-align-justify" />
@@ -43,21 +68,29 @@ export default function CreateQuestions() {
           </div>
 
           <div className=" flex justify-content-center">
-            <Button id="botoes" label="0 a 10" onClick={() => setType('0_a_10')} />
-            <Button id="botoes" label="Alternativa" onClick={() => { setType('alternativa'); setAlt(true); }} />
-            <Dialog id="alt" header="Alternativa" headerStyle={{ position: 'relative', textAlign: 'center' }} justifyContent="center" visible={alt} maximizable style={{ width: '50vw' }} onHide={() => setAlt(false)}>
-              <div className=" flex justify-content-center gap-3">
-                <InputText placeholder="Opcão" />
-                <Button id="adicao" icon="pi pi-plus" rounded outlined aria-label="Filter" />
+            <Button id="botoes" label="0 a 10" onClick={() => { setType('0_a_10'); setSave(false); }} />
+            <Button id="botoes" label="Alternativa" onClick={() => { setType('alternativa'); setAlt(true); setSave(false); }} />
+            <Dialog id="alt" header="Alternativas" headerStyle={{ textAlign: 'center' }} justifyContent="center" visible={alt} maximizable style={{ width: '50vw' }} onHide={() => setAlt(false)}>
+              {alternativas.map((alternativa, index) => (
+                <div className=" flex justify-content-center" key={alternativa.id}>
+                  <Spans icon="pi pi-pencil" />
+                  <InputText placeholder="Opcão" style={{ width: '500px' }} value={alternativa.value} onChange={(event) => handleAlternativaChange(event, alternativa.id)} />
+                  <Button icon="pi pi-delete-left" type="button" rounded outlined aria-label="Filter" style={{ marginLeft: '10px' }} onClick={() => handleDeleteAlternativa(index)} />
+                </div>
+              ))}
+              <div className="flex justify-content-center gap-3" style={{ margin: '10px' }}>
+                <Button label="Cancelar" onClick={() => { setSave(true); setAlt(false); setAlternativas([]); }} />
+                <Button icon="pi pi-plus" type="button" rounded outlined aria-label="Filter" style={{ marginLeft: '10px' }} onClick={addAlternativa} label="Adicionar Opção" />
+                <Button label="Salvar" disabled={vazio} onClick={() => setAlt(false)} />
               </div>
             </Dialog>
-            <Button id="botoes" label="Múltipla Escolha" onClick={() => { setType('multipla_escolha'); setMult(true); }} />
+            <Button className="type-button" id="botoes" label="Múltipla Escolha" onClick={() => { setType('multipla_escolha'); setMult(true); setSave(false); }} />
             <Dialog id="mult" header="Múltipla Escolha" headerStyle={{ position: 'relative', textAlign: 'center' }} justifyContent="center" visible={mult} maximizable style={{ width: '50vw' }} onHide={() => setMult(false)}>
               <div>
                 <InputText placeholder="teste2" />
               </div>
             </Dialog>
-            <Button id="botoes" label="Aberta" onClick={() => setType('aberta')} />
+            <Button className="type-button" id="botoes" label="Aberta" onClick={() => { setType('aberta'); setSave(false); }} />
           </div>
         </div>
       </div>
@@ -70,9 +103,9 @@ export default function CreateQuestions() {
         }}
       >
 
-        <Button id="botao_limpar" label="Limpar" icon="pi pi-check" />
-        <Button label="Listar" icon="pi pi-check" />
-        <Button label="Salvar" icon="pi pi-check" />
+        <Button label="Limpar" />
+        <Button label="Listar" />
+        <Button label="Salvar" disabled={save || isTitleEmpty} />
       </div>
     </div>
   );
