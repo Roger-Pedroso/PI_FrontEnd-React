4e16c036ef2cdc0666fcdfdf9d5a7c4b71b86be3
Bruno <brunoalvesambrosio@gmail.com>
2023-06-02 22:19:48 -0300

    esqueleto do contexto

--- frontend/src/context/Login/KeyContext.jsx
+++ frontend/src/context/Login/KeyContext.jsx
@@ -1,17 +1,73 @@
 import React, { createContext, useMemo, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import api from '../../utils/Api';
 
 const KeyContext = createContext();
 
 function KeyProvider({ children }) {
-  const [key, setKey] = useState({});
-  const [answer, setAnswer] = useState();
+  const navigate = useNavigate();
+  const [key, setKey] = useState('');
+  const [quiz, setQuiz] = useState({});
+  const [keyId, setKeyId] = useState(JSON.stringify(sessionStorage.getItem('keyId')));
+
+  const saveKeyId = () => {
+    sessionStorage.setItem('keyId', { keyId });
+  };
+
+  const getKeyId = async () => {
+    try {
+      await api.get(`/key/${key}`).then((response) => {
+        const { data } = response;
+        setKeyId(data.id);
+        saveKeyId();
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const sendToQuiz = () => {
+    navigate(`/app/quizes/answer/${quiz.id}`);
+  };
+
+  const getQuiz = async () => {
+    try {
+      await api.get(`/key/${key}`).then((response) => {
+        const { data } = response;
+        setQuiz(data.quiz);
+        getKeyId();
+        saveKeyId();
+        sendToQuiz();
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const killKey = async () => {
+    try {
+      await api.put(`/api/${keyId}`).then(() => {
+        console.log('chave desativada.');
+        sessionStorage.removeItem('keyId');
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   const memo = useMemo(() => ({
     key,
     setKey,
-    answer,
-    setAnswer,
-  }), [key, setKey, answer, setAnswer]);
+    quiz,
+    setQuiz,
+    keyId,
+    setKeyId,
+    getQuiz,
+    getKeyId,
+    sendToQuiz,
+    killKey,
+    saveKeyId,
+  }), [key, setKey, quiz, setQuiz, keyId, getQuiz]);
 
   return (
     <KeyContext.Provider value={memo}>
