f88efd55c2cfe6219a158d82238bc113dd19ea4a
yNanneRy <renangealves@gmail.com>
2023-06-05 20:27:17 -0300

    ajustes no keycontext

--- frontend/src/context/Login/KeyContext.jsx
+++ frontend/src/context/Login/KeyContext.jsx
@@ -10,6 +10,7 @@ function KeyProvider({ children }) {
 
   const saveKeyId = (id) => {
     sessionStorage.setItem('keyId', id);
+    sessionStorage.setItem('key', key);
   };
 
   const sendToQuiz = (id) => {
@@ -41,6 +42,7 @@ function KeyProvider({ children }) {
       await api.put(`/key/${sessionStorage.getItem('keyId')}`).then(() => {
         sessionStorage.removeItem('keyId');
         sessionStorage.removeItem('auth-key');
+        sessionStorage.removeItem('key');
         setTimeout(() => {
           navigate('/');
           window.location.reload();
