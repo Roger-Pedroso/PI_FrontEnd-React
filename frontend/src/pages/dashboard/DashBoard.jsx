a705edb46d7307b1d96bcdd79cd4212cb262b82d
Bruno <brunoalvesambrosio@gmail.com>
2023-06-18 16:58:22 -0300

    ajustes na responsividade

--- frontend/src/pages/dashboard/DashBoard.jsx
+++ frontend/src/pages/dashboard/DashBoard.jsx
@@ -60,40 +60,42 @@ export default function DashBoard() {
   }, []);
   return (
     <div style={innerWidth > 600 ? { margin: '50px' } : {}}>
-      <div className="card flex gap-3">
-        <div className="flex flex-column gap-3">
-          <div>
-            <Dropdown
-              value={chartType}
-              onChange={(e) => setChartType(e.value)}
-              optionLabel="nome"
-              placeholder="Superior Imediato"
-              className="w-full md:w-14rem"
-            />
+      <div className="card flex flex-wrap gap-3 justify-content-between flex-row-reverse">
+        <div className="flex flex-column" style={{ textAlign: 'end', marginRight: '5px' }}>
+          <h3>Nome do Superior</h3>
+          <h3>Nome do Questionário</h3>
+        </div>
+        <div className="flex gap-3 flex-wrap">
+          <div className="flex flex-column gap-3">
+            <div>
+              <Dropdown
+                value={chartType}
+                onChange={(e) => setChartType(e.value)}
+                optionLabel="nome"
+                placeholder="Superior Imediato"
+                className="w-full md:w-14rem"
+              />
+            </div>
+            <div>
+              <Dropdown
+                placeholder="Tipo de Questão"
+                className="w-full md:w-14rem"
+                disabled
+              />
+            </div>
           </div>
           <div>
             <Dropdown
-              placeholder="Tipo de Questão"
+              placeholder="Questionário"
               className="w-full md:w-14rem"
               disabled
             />
           </div>
         </div>
-        <div>
-          <Dropdown
-            placeholder="Questionário"
-            className="w-full md:w-14rem"
-            disabled
-          />
-        </div>
-        <div className="flex-column" style={{ textAlign: 'end', width: '100%', marginRight: '5px' }}>
-          <h3>Nome do Superior</h3>
-          <h3>Nome do Questionário</h3>
-        </div>
       </div>
-      <div className="card flex p-5 justify-content-between">
+      <div className={innerWidth > 600 ? 'card flex flex-wrap justify-content-between' : 'card flex flex-wrap justify-content-center'}>
         {questions.map((item) => (
-          <div><Chart key={item.id} type={chartType} data={chartData} options={chartOptions} className="w-full md:w-25rem" /></div>
+          <div className="flex justify-content-center p-5"><Chart key={item.id} type={chartType} data={chartData} options={chartOptions} className="flex w-full md:w-20rem" /></div>
         ))}
       </div>
     </div>
