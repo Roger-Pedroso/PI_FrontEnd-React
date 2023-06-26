63c6318853665bdd90d5e03466268cf6f5ef1e52
Bruno <brunoalvesambrosio@gmail.com>
2023-06-26 00:17:08 -0300

    dashboard de alternativas finalizado.

--- frontend/src/pages/dashboard/DashBoard.jsx
+++ frontend/src/pages/dashboard/DashBoard.jsx
@@ -1,21 +1,17 @@
+/* eslint-disable no-param-reassign */
 /* eslint-disable no-restricted-globals */
 import React, { useEffect, useState } from 'react';
 import { Chart } from 'primereact/chart';
 import { Dropdown } from 'primereact/dropdown';
-import { Button } from 'primereact/button';
 import api from '../../utils/Api';
 
 export default function DashBoard() {
   const [superior, setSuperior] = useState([]);
-  const [question, setQuestion] = useState([]);
-  const [selectedQuestion, setSelectedQuestion] = useState({});
   const [selectedSuperior, setSelectedSuperior] = useState({});
   const [quiz, setQuiz] = useState([]);
   const [selectedQuiz, setSelectedQuiz] = useState([]);
   const [altData, setAltData] = useState([]);
-  const [altDataConv, setAltDataConv] = useState([]);
-
-  const [chartData, setChartData] = useState({});
+  const [alternativas, setAlternativas] = useState([]);
   const [chartOptions, setChartOptions] = useState({});
 
   const findSuperior = async () => {
@@ -35,26 +31,21 @@ export default function DashBoard() {
         const filteredQuizzes = response.data.filter(
           (item) => item.superior.id === selectedSuperior.id,
         );
-        setQuiz(filteredQuizzes.map((item) => item.quiz));
+        const quizzes = filteredQuizzes.map((item) => item.quiz);
+        const excludeRepetition = Object.values(quizzes.reduce((accumulator, obj) => {
+          accumulator[obj.id] = obj;
+          return accumulator;
+        }, {}));
+        setQuiz(excludeRepetition);
       });
     } catch (err) {
       console.log(err);
     }
   };
 
-  const findQuestion = async () => {
-    try {
-      const questionsMapped = selectedQuiz.questions;
-      setQuestion(questionsMapped);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
   const getDashBoardData = async () => {
-    console.log(altData);
     try {
-      await api.get(`relatorio/${selectedQuiz.id}`).then((response) => {
+      await api.get(`relatorio-alternativas/${selectedQuiz.id}`).then((response) => {
         const { data } = response;
         setAltData(data);
       });
@@ -64,21 +55,37 @@ export default function DashBoard() {
   };
 
   const convertDashBoardData = () => {
-    const data = altData.map(([alternativa, valor]) => ({
+    const data = altData.map(([alternativa, valor, idQuestao, nome]) => ({
       alternativa,
       valor: String(valor),
+      idQuestao,
+      nome,
     }));
-    setAltDataConv(data);
+    const grupos = data.reduce((acumulator, resposta) => {
+      const {
+        nome, idQuestao, alternativa, valor,
+      } = resposta;
+      if (acumulator[idQuestao]) {
+        acumulator[idQuestao].alternativas.push(alternativa);
+        acumulator[idQuestao].valores.push(valor);
+      } else {
+        acumulator[idQuestao] = {
+          nome,
+          idQuestao,
+          alternativas: [alternativa],
+          valores: [valor],
+        };
+      }
+      return acumulator;
+    }, {});
+
+    setAlternativas(Object.values(grupos));
   };
 
   useEffect(() => {
     convertDashBoardData();
-    console.log(altDataConv);
+    console.log(alternativas);
   }, [altData]);
-
-  useEffect(() => {
-    findQuestion();
-  }, [selectedQuiz]);
 
   useEffect(() => {
     findQuiz();
@@ -86,24 +93,14 @@ export default function DashBoard() {
   }, [selectedSuperior]);
 
   useEffect(() => {
-    if (selectedSuperior || selectedQuiz || selectedQuestion) {
-      getDashBoardData();
-    }
-  }, [selectedQuestion]);
+    getDashBoardData();
+  }, [selectedQuiz]);
 
   useEffect(() => {
-    if (superior.length === 0) {
-      findSuperior();
-    }
+    findSuperior();
+  }, [selectedSuperior]);
 
-    const data = {
-      labels: altDataConv.map((item) => item.alternativa),
-      datasets: [
-        {
-          data: altDataConv.map((item) => item.valor),
-        },
-      ],
-    };
+  useEffect(() => {
     const options = {
       plugins: {
         legend: {
@@ -114,15 +111,8 @@ export default function DashBoard() {
       },
     };
 
-    setChartData(data);
     setChartOptions(options);
-  }, [superior]);
-
-  const clearFilter = () => {
-    setSelectedSuperior('');
-    setSelectedQuiz('');
-    setSelectedQuestion('');
-  };
+  }, [alternativas]);
 
   return (
     <div style={innerWidth > 600 ? { margin: '50px' } : {}}>
@@ -140,27 +130,15 @@ export default function DashBoard() {
           </h3>
         </div>
         <div className="flex gap-3 flex-wrap">
-          <div className="flex flex-column gap-3">
-            <div>
-              <Dropdown
-                value={selectedSuperior}
-                onChange={(e) => setSelectedSuperior(e.value)}
-                optionLabel="nome"
-                options={superior}
-                placeholder="Superior Imediato"
-                className="w-full md:w-14rem"
-              />
-            </div>
-            <div>
-              <Dropdown
-                value={selectedQuestion}
-                onChange={(e) => setSelectedQuestion(e.value)}
-                optionLabel="nome_campo"
-                options={question}
-                placeholder="Questão"
-                className="w-full md:w-14rem"
-              />
-            </div>
+          <div>
+            <Dropdown
+              value={selectedSuperior}
+              onChange={(e) => setSelectedSuperior(e.value)}
+              optionLabel="nome"
+              options={superior}
+              placeholder="Superior Imediato"
+              className="w-full md:w-14rem"
+            />
           </div>
           <div className="flex flex-column gap-3">
             <div>
@@ -173,17 +151,37 @@ export default function DashBoard() {
                 className="w-full md:w-14rem"
               />
             </div>
-            <div className="flex align-self-end">
-              <Button label="Limpar Filtros" onClick={() => clearFilter()} />
-            </div>
           </div>
         </div>
       </div>
-      <div className={innerWidth > 600 ? 'card flex flex-wrap justify-content-between' : 'card flex flex-wrap justify-content-center'}>
-        {altDataConv.map((item) => (
-          <div className="flex justify-content-center p-5"><Chart key={item.id} type="pie" data={chartData} options={chartOptions} className="flex w-full md:w-20rem" /></div>
-        ))}
+      <div className="card flex-column justify-content-center" style={{ textAlign: 'center' }}>
+        <h1>Questões</h1>
+        <div className={innerWidth > 600 ? 'flex flex-wrap justify-content-between' : 'card flex flex-wrap justify-content-center'}>
+          {alternativas.map((item) => (
+            <div className="flex justify-content-center p-5" key={item.idQuestao}>
+              <div className="flex flex-column" style={{ textAlign: 'center' }}>
+                <h3>
+                  {item.nome}
+                </h3>
+                <Chart
+                  type="pie"
+                  data={{
+                    labels: item.alternativas,
+                    datasets: [
+                      {
+                        data: item.valores,
+                      },
+                    ],
+                  }}
+                  options={chartOptions}
+                  className="flex w-full md:w-20rem"
+                />
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
+
     </div>
   );
 }
